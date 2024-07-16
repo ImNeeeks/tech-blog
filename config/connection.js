ba429a0299489457c04199c6311d97a2c0636c63
Nikko <nikkojacinto@gmail.com>
2024-07-15 20:25:23 -0700

    testing connection change

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,38 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false, // Disable logging; default: console.log
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // You may need to change this based on your environment
+      },
+    },
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASS, // Changed from DB_PW to DB_PASS
     {
       host: 'localhost',
       dialect: 'postgres',
+      logging: false, // Disable logging; default: console.log
     },
   );
 }
+
+// Test connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
