07f21bedc7c99de1ec185215b7dd3bd820c42d7e
Nikko <nikkojacinto@gmail.com>
2024-07-15 20:43:25 -0700

    reverting back to original connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,43 +1,22 @@
 // Connection file should allow application to be hosted locally and on Render
 
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('.env').config();
 
 let sequelize;
 
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    logging: false, // Disable logging; default: console.log
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // You may need to change this based on your environment
-      },
-    },
-  });
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS, // Changed from DB_PW to DB_PASS
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'postgres',
-      logging: false, // Disable logging; default: console.log
     },
   );
 }
-
-// Test connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 module.exports = sequelize;
